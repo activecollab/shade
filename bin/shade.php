ae6389588ef255b04fd2c421fe2e623b39ff6279
Ilija Studen <ilija.studen@activecollab.com>
2019-01-01 19:38:43 +0100

    Improve code style

--- bin/shade.php
+++ bin/shade.php
@@ -6,13 +6,17 @@
  * (c) A51 doo <info@activecollab.com>. All rights reserved.
  */
 
+use ActiveCollab\Shade\Bootstrap\BootstrapApplication;
+
 date_default_timezone_set('UTC');
 
 define('SHADE_ROOT_PATH', dirname(__DIR__));
 
 require SHADE_ROOT_PATH . '/vendor/autoload.php';
 
-(new \ActiveCollab\Shade\Bootstrap\BootstrapApplication(
+(new BootstrapApplication(
     trim(file_get_contents(SHADE_ROOT_PATH . '/VERSION')),
     SHADE_ROOT_PATH . '/src/ActiveCollab/Shade/Command'
-))->bootstrapApp()->run();
+))
+    ->bootstrapApp()
+        ->run();
